e4b70ef9f2e58d7be394a1c53bafe5f0f9e58889
Rommel Fowler <info@rommelfowler.com>
2018-08-31 09:33:31 -0700

    updated plugin -> counters

---
+++ template-parts/folio-parts/content-pr.php
@@ -0,0 +1,45 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 no-padding-lr xs-padding-15px-lr">
+            <div class="filter-content overflow-hidden">
+                <ul class="portfolio-grid portfolio-metro-grid work-4col hover-option8 gutter-medium">
+                    <li class="grid-sizer"></li>
+                    <?php $postQuery = new WP_Query( array( 'post_type' => 'folio', 'posts_per_page'=>'-1','orderby' => 'post_id', 'order' => 'DESC' ) );
+                          if($postQuery->have_posts() ) : while($postQuery->have_posts() ) : $postQuery->the_post();
+                          $grid_item_size = get_field('grid_item_size');
+                          $grid_item_delay = get_field('grid_item_delay');
+                          $grid_menu_item = get_field('grid_menu_item');
+                          $category = get_the_category();
+                          $firstCategory = $category[0]->cat_name;
+                          ?>
+
+                    <!-- start portfolio item -->
+                    <li class="<?php echo $grid_item_size; ?> <?php echo implode(' ' , $grid_menu_item); ?> wow fadeInUp" data-wow-delay="<?php echo $grid_item_delay; ?>">
+                        <a href="<?php the_permalink(); ?>">
+                            <figure>
+                              <?php if( has_post_thumbnail() ) :  ?>
+                                <div class="portfolio-img"><?php the_post_thumbnail(); ?></div>
+                              <?php else: ?>
+                                <div class="portfolio-img"><img src="https://placehold.it/900x900" alt="placeholder image"/></div>
+                              <?php endif; ?>
+                                <figcaption>
+                                    <div class="portfolio-hover-main">
+                                        <div class="portfolio-hover-box vertical-align-middle">
+                                            <div class="portfolio-icon alt-font font-weight-600 icon-medium">+</div>
+                                            <span class="text-extra-large alt-font text-extra-dark-gray margin-5px-bottom"><?php the_title(); ?></span>
+                                            <p class="no-letter-spacing text-small no-margin-bottom text-medium-gray"><?php echo $firstCategory; ?></p>
+                                        </div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </li>
+                    <!-- end portfolio item -->
+
+                  <?php endwhile; endif; wp_reset_query();?>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
